071126e1dba08da242280e846cb4b52ed3812c0c
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2026-06-19 19:48:24 +0530

    Add: find-the-difference - submission-1

---
+++ Data Structures & Algorithms/find-the-difference/submission-1.py
@@ -0,0 +1,26 @@
+# Convert a lowercase letter
+# print(ord('a'))   # Output: 97
+# The inverse: converting an ASCII code back to text
+# print(chr(65))    # Output: 'A' # note that it's CHR AND NOT CHAR
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        if not s : return t
+        if not t : return s
+        # so now both s and t has some characters 
+        xor = 0
+        for char in s:
+            xor ^= (ord(char)) 
+        for char in t:
+            xor ^= (ord(char)) 
+        return chr(xor)
+
+        # Another way : 
+        # res = 0 
+        # for char in s:
+        #     res -= ord(char)
+        # for char in t:
+        #     res += ord(char)
+        # return chr(res)
